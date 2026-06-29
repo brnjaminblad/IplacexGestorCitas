eae8a3cad3b9856fa4923ea34c02dffe51da1695
brnjaminblad <brunnoben@gmail.com>
2026-06-29 11:29:05 -0400

    Update PATH V2

--- quoteApp/src/app/app.routes.ts
+++ quoteApp/src/app/app.routes.ts
@@ -2,24 +2,26 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
   },
+
   {
     path: 'home',
-    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+    loadComponent: () =>
+      import('./pages/home/home.page').then(m => m.HomePage),
   },
+
   {
     path: 'quotes',
-    loadComponent: () => import('./pages/quotes/quotes.page').then( m => m.QuotesPage)
+    loadComponent: () =>
+      import('./pages/quotes/quotes.page').then(m => m.QuotesPage),
   },
+
   {
     path: 'settings',
-    loadComponent: () => import('./pages/settings/settings.page').then( m => m.SettingsPage)
+    loadComponent: () =>
+      import('./pages/settings/settings.page').then(m => m.SettingsPage),
   },
 ];
---
+++ quoteApp/src/app/components/quote-card/quote-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-card',
+  templateUrl: './quote-card.component.html',
+  styleUrls: ['./quote-card.component.scss'],
+})
+export class QuoteCardComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
