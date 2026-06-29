e9d269f6b1d74211ab9e27c192555ee30df557b3
brnjaminblad <brunnoben@gmail.com>
2026-06-29 11:22:34 -0400

    Initial Insert Paths on GITHUB

---
+++ quoteApp/src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'quotes',
+    loadComponent: () => import('./pages/quotes/quotes.page').then( m => m.QuotesPage)
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./pages/settings/settings.page').then( m => m.SettingsPage)
+  },
+];
