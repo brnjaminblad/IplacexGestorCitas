a706c47aeddef04c28937726c0c959d354c93b55
brnjaminblad <brunnoben@gmail.com>
2026-06-29 10:18:26 -0400

    Create project architecture

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,28 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'quotes',
+    loadComponent: () => import('./pages/quotes/quotes.page').then( m => m.QuotesPage)
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./pages/settings/settings.page').then( m => m.SettingsPage)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'quotes',
+    loadComponent: () => import('./pages/quotes/quotes.page').then( m => m.QuotesPage)
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./pages/settings/settings.page').then( m => m.SettingsPage)
+  },
 ];
---
+++ src/app/models/quote.ts
@@ -0,0 +1,5 @@
+export interface Quote {
+  id?: number;
+  phrase: string;
+  author: string;
+}
