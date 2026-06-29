a1d5d0fdaa5a62a11d9475f863d5dae177f685e5
brnjaminblad <brunnoben@gmail.com>
2026-06-29 11:42:20 -0400

    Added some Touch v2

--- quoteApp/src/app/app.routes.ts
+++ quoteApp/src/app/app.routes.ts
@@ -1,24 +1,18 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
 
   {
     path: 'home',
     loadComponent: () =>
       import('./pages/home/home.page').then(m => m.HomePage),
   },
-
   {
     path: 'quotes',
     loadComponent: () =>
       import('./pages/quotes/quotes.page').then(m => m.QuotesPage),
   },
-
   {
     path: 'settings',
     loadComponent: () =>
